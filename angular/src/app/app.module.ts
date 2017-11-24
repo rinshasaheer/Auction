48054f4489c30721f8dc5504127273f2652bc606
ScottMachlovski <prazad.manu@gmail.com>
2017-11-24 09:10:22 +0530

    front end

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,16 +4,23 @@ import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { CardComponent } from './card/card.component';
 
 const appRoutes: Routes = [
   {path:'', redirectTo:'/registration', pathMatch:'full'},
   {path:'registration', component:RegistrationComponent},
+  {path:'home', component:HomeComponent},
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    NavigationComponent,
+    HomeComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
